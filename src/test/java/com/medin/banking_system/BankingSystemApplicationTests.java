41b084ffb7a32dbae4943a79197284e7cd484477
jesusmedina <medina.a300@outlook.com>
2025-06-25 14:36:26 -0700

    chore: initial commit of Spring Boot project

---
+++ src/test/java/com/medin/banking_system/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medin.banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
